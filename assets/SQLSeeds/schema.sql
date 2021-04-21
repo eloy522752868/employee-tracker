f0c10d2482e71209406f14f4c6e0028c77cdd1d7
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-20 23:51:25 -0400

    need make changes to my schema file

--- assets/SQLSeeds/schema.sql
+++ assets/SQLSeeds/schema.sql
@@ -1,9 +1,7 @@
 DROP DATABASE IF EXISTS employee_tracker;
 
--- Create easy bay database --
 CREATE DATABASE employee_tracker;
 
-  -- Open greateby table
 USE  employee_tracker;
 
   -- create table department
@@ -19,11 +17,11 @@ CREATE TABLE role
 (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
   id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  -- Makes a string column called "item_name" which cannot contain null --
+
   title VARCHAR(30) NOT NULL,
-  -- Makes a string column called "category" which cannot contain null --
+
   salary  DECIMAL,
-  -- Makes an numeric column called "starting_bid" --
+
   department_id  INT
 )
   -- create table employee
@@ -31,12 +29,10 @@ CREATE TABLE employee
 (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
   id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  -- Makes a string column called "item_name" which cannot contain null --
   first_name VARCHAR(30) NOT NULL,
-  -- Makes a string column called "category" which cannot contain null --
   last_name VARCHAR(30) NOT NULL,
-  -- Makes an numeric column called "starting_bid" --
+
   role_id INT,
-  -- Makes an numeric column called "highest_bid" --
+
   manager_id  INT
 )
